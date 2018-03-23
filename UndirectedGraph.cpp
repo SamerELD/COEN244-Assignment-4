65636baa8f177ed441644470ef42642654bfcec4
SamerELD <37485858+SamerELD@users.noreply.github.com>
2018-03-22 23:58:50 -0400

    Update UndirectedGraph.cpp

--- UndirectedGraph.cpp
+++ UndirectedGraph.cpp
@@ -45,12 +45,67 @@ bool UndirectedGraph::removeVertex(Vertex &v)
     {
         if(VertexVector[i].getID() == v.getID())
         {
+
+        	for(unsigned int j = 0; j < EdgesVector.size(); j++)
+        	{
+        		if(EdgesVector[j].getEndingVertex().getID() == v.getID())
+        		{
+        			EdgesVector.erase(EdgesVector.begin() + j);
+        		}
+        	}
+
             VertexVector.erase(VertexVector.begin() + i);
             return true;
         }
     }
     return false;
 }
+
+
+bool UndirectedGraph::addEdge(Edge &e)
+{
+	EdgesVector.push_back(e);
+	return true;
+}
+
+bool UndirectedGraph::remove(Edge &e)
+{
+	 for(unsigned int i = 0; i < EdgesVector.size(); i++)
+	    {
+	        if(EdgesVector[i].getWeight() == e.getWeight())
+	        {
+	            EdgesVector.erase(EdgesVector.begin() + i);
+	            return true;
+	        }
+	    }
+	    return false;
+}
+
+
+bool UndirectedGraph::searchVertex(Vertex &v)
+{
+	for(unsigned int i = 0; i < VertexVector.size(); i++)
+	{
+		if(VertexVector[i].getID() == v.getID())
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool UndirectedGraph::searchEdge(Edge &e)
+{
+	for(unsigned int i = 0; i < EdgesVector.size(); i++)
+	{
+		if(EdgesVector[i].getWeight() == e.getWeight())
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 
 
 
